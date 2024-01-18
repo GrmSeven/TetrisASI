8f4b5ba05346c50f7945e3a12cf3e1ea745a98b8
GrmSeven <grmseven7@gmail.com>
2024-01-18 15:11:13 +0200

    Delta time and unnecessary stuff

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ class Tetris:
     def __init__(self, size):
         self.shapes = []
         self.size = size
-        self.matrix = [[0] * size[0]] * size[1]
+        self.matrix = [[0] * size[0] for i in range(size[1])]
+        self.timer = time.time()
         self.state = "Running"  # Or paused
+
+    def update_timer(self, delta):
+        if time.time() - self.timer >= delta:
+            self.timer = time.time()
+            return 1
+        return 0
 
     def add_new_shape(self, matrix, rarity):
         self.shapes.append([matrix, rarity])
@@ -20,46 +27,47 @@ class Tetris:
         os.system('cls')
         
     def render(self):
+        cell_icon = ["   ", "[.]"]
         self.clear_console()
         for y, row in enumerate(game.matrix):
             for x, cell in enumerate(row):
-                if cell == 0:
-                    print(".", end="")
-                else:
-                    print("*", end="")
+                print(cell_icon[cell], end="")
             print("\n", end="")
-
-    def test_update(self):
-        self.matrix[5][2] += 1
 
 
 if __name__ == "__main__":
     game = Tetris([10, 20])
-    game.add_new_shape([[0, 1, 0],
-                    [1, 1, 1],
-                    [0, 1, 0]], 1)
-    game.add_new_shape([[0, 0, 0],
-                    [0, 1, 1],
-                    [0, 1, 0]], 1)
-    game.add_new_shape([[0, 0, 0],
-                    [1, 1, 1],
-                    [1, 1, 1]], 1)
-    game.add_new_shape([[0, 1, 0],
-                    [1, 1, 1],
-                    [1, 0, 0]], 1)
-    game.add_new_shape([[0, 0, 1, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 1, 0]], 1)
-    game.add_new_shape([[1, 1, 0],
-                    [0, 1, 0],
-                    [1, 1, 1]], 1)
-    game.add_new_shape([[1, 1, 1],
-                    [1, 0, 1],
-                    [0, 0, 0]], 1)
+    timer = 0
+    initial_tetrominos = [
+        [[[0, 1, 0],
+          [1, 1, 1],
+          [0, 1, 0]], 1],
+        [[[0, 0, 0],
+          [0, 1, 1],
+          [0, 1, 0]],1],
+        [[[0, 0, 0],
+          [1, 1, 1],
+          [1, 1, 1]], 1],
+        [[[0, 1, 0],
+          [1, 1, 1],
+          [1, 0, 0]], 1],
+        [[[0, 0, 1, 0],
+          [0, 0, 1, 0],
+          [0, 0, 1, 0],
+          [0, 0, 1, 0]], 1],
+        [[[1, 1, 0],
+          [0, 1, 0],
+          [1, 1, 1]], 1],
+        [[[1, 1, 1],
+          [1, 0, 1],
+          [0, 0, 0]], 1]
+    ]
+    for tetromino in initial_tetrominos:
+        print(tetromino)
+        game.add_new_shape(*tetromino)
+
     # Game loop
     while True:
-        game.test_update()
-        game.render()
-        time.sleep(1)
-
+        if game.update_timer(1):
+            game.matrix[random.randint(0, game.size[1]-1)][random.randint(0, game.size[0]-1)] = 1
+            game.render()
