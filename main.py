c930847776286257e0f68c3043c90375afc674e8
GrmSeven <grmseven7@gmail.com>
2024-02-07 18:56:18 +0200

    Restarting added

--- main.py
+++ main.py
@@ -45,6 +45,27 @@ class Tetris:
     def exit(self):
         self.state = -1
 
+    def reset(self):
+        # Constants
+        self.shapes = []
+        self.size = [10, 20]
+        self.square_size = 40
+        # Yes
+        self.matrix = [[0] * (self.size[0]) for i in range(self.size[1])]
+        self.prev_matrix = deepcopy(self.matrix)
+        self.curr_tetromino = []
+        self.tetromino_position = [0, 0]
+        self.curr_color = 1
+        # Ticking
+        self.timer = time.time()
+        self.tick = 0
+        self.is_next_tick = True
+        # Game state
+        self.skip_next_frame = False
+        self.score = 0
+        self.state = 1
+        self.move_vector = None  # [-1, 0]
+
     def update_timer(self, delta):
         if self.state == 1:
             if time.time() - self.timer >= delta:
@@ -165,10 +186,20 @@ class Tetris:
         canvas.create_rectangle(1*s, 6*s, 8*s+s, 11*s+s, fill="#f74f43", outline="")
         canvas.create_text(200, 320, text=f"Game Over\n  Score:", font=("Terminal", 40, "bold"))
         canvas.create_text(200, 420, text=self.score, font=("Terminal", 40, "bold"), anchor=tkinter.CENTER)
+        canvas.create_rectangle(200-110, 530-30, 200+110, 530+30, fill="#000000", outline="")
+        canvas.create_text(200, 530, text="Press  Space\n to restart", font=("Terminal", 20, "bold"), fill="#ffffff", anchor=tkinter.CENTER)
 
     def print_debug(self):
         print(self.curr_tetromino)
         print(self.tetromino_position)
+
+def create_game():
+    global game
+    game = Tetris()
+    for tetromino in initial_tetrominos:
+        game.add_new_shape(*tetromino)
+    game.decide_next_tetromino()
+    game.render(True)
 
 if __name__ == "__main__":
     # Tkinter canvas
@@ -178,7 +209,6 @@ if __name__ == "__main__":
     canvas.config(width=400, height=800)
 
     # Tetris initialisation
-    game = Tetris()
     initial_tetrominos = [
         [[[0, 1, 0],           #   |*|
           [1, 1, 1],           # |*|*|*|
@@ -209,11 +239,7 @@ if __name__ == "__main__":
           [1, 0, 1],           # |*| |*|
           [0, 0, 0]], 1, 4]    #
     ]
-    for tetromino in initial_tetrominos:
-        game.add_new_shape(*tetromino)
-
-    game.decide_next_tetromino()
-    game.render(True)
+    create_game()
 
     def input_set(x):
         global next_input
@@ -225,6 +251,7 @@ if __name__ == "__main__":
     keyboard.add_hotkey('e', lambda: input_set('e'))
     keyboard.add_hotkey('q', lambda: input_set('q'))
     keyboard.add_hotkey('w', lambda: input_set('w'))
+    keyboard.add_hotkey('space', lambda: input_set('space'))
     input_commands = {'a': lambda: game.move_tetromino("L"),
                       'd': lambda: game.move_tetromino("R"),
                       's': lambda: game.move_tetromino("D"),
@@ -234,24 +261,31 @@ if __name__ == "__main__":
 
     input_set("")
     while True:  # Game loop
-        if next_input != "":
-            input_commands[next_input]()
-            next_input = ""
-            game.draw_tetromino()
-            game.render()
-            game.draw_tetromino(True)
-        if game.is_next_tick:
-            if not game.move_tetromino('D'):
+        if game.state == 0 and next_input == "space":
+            create_game()
+
+        if game.state == 1:
+            if next_input in ['w', 'a', 's', 'd', 'q', 'e']:
+                input_commands[next_input]()
+                next_input = ""
                 game.draw_tetromino()
-                game.check_row()
-                game.decide_next_tetromino()
-                if game.check_collision(game.tetromino_position, game.curr_tetromino):
-                    game.show_score()
-                    game.exit()
-            game.draw_tetromino()
-            game.render()
-            game.draw_tetromino(True)
-        game.update_timer(0.5)
+                game.render()
+                game.draw_tetromino(True)
+            if game.is_next_tick:
+                if not game.move_tetromino('D'):
+                    game.draw_tetromino()
+                    game.check_row()
+                    game.decide_next_tetromino()
+                    if game.check_collision(game.tetromino_position, game.curr_tetromino):
+                        game.show_score()
+                        game.pause()
+                game.draw_tetromino()
+                game.render()
+                game.draw_tetromino(True)
+            game.update_timer(0.5)
+
         if game.state == -1:
             break
+
+        canvas.update()
     root.mainloop()
